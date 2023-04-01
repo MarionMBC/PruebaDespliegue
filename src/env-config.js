15284a649a9c37b5cfaeaa03822a76729677207a
MarionMBC <melchisedec.bustamante@unah.hn>
2023-04-01 08:58:54 -0600

    first commit

--- src/env-config.js
+++ src/env-config.js
@@ -1,15 +1,15 @@
-import { config } from "dotenv";
-config();
-
-
-
-export const DB_PORT=  process.env.DB_PORT;
-export const DB_HOST= process.env.DB_HOST;
-export const DB_USER= process.env.DB_USER;
-export const DB_PASSWORD= process.env.DB_PASSWORD;
-export const DB_NAME= process.env.DB_NAME;
-export const SERVER_PORT= process.env.SERVER_PORT;
-
-export const DB_SSL = process.env.SSl;
-
-
+// import { config } from "dotenv";
+// config();
+//
+//
+//
+// export const DB_PORT=  process.env.DB_PORT;
+// export const DB_HOST= process.env.DB_HOST;
+// export const DB_USER= process.env.DB_USER;
+// export const DB_PASSWORD= process.env.DB_PASSWORD;
+// export const DB_NAME= process.env.DB_NAME;
+// export const SERVER_PORT= process.env.SERVER_PORT;
+//
+// export const DB_SSL = process.env.SSl;
+//
+//
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import app from "./app.js";
-import { SERVER_PORT } from "./env-config.js";
+// import { SERVER_PORT } from "./env-config.js";
 
 
 /** 
  ** Inicializa el servidor
 */
 app.listen( 4000, ()=>{
-    console.log("Servidor inicializado en el puerto", SERVER_PORT);
+    console.log("Servidor inicializado en el puerto", 4000);
 });
 
